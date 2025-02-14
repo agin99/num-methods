054b16484714942fd9ef5afd9a9de373fe4aa6e5
agin99 <alan.gindi99@gmail.com>
2025-02-13 19:57:43 -0500

    Plotting terminal velocity func

--- chapter-1/falling_parachutist.py
+++ chapter-1/falling_parachutist.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 """
 Problem statement: A parachutist of mass 68.1 kg jumps out of a stationary
@@ -21,11 +22,21 @@ step = 2 #s
 def v_gen():
     vi = 0
     vf = g*step
+    v_list = []
     temp_vi = 0
-    while vf - vi >= 0.001 * vi: 
+    while vf - vi >= 0.0000001 * vi: 
         vi = temp_vi
         vf = vi + (g - c*vi/m)*step
+        v_list.append(vf)
         temp_vi = vf
-    return vf
+    return vf, v_list
 
-print(f"Terminal velocity: {v_gen():.2f}")
+v_t, v_list = v_gen()
+t = [2*i for i in range(len(v_list))]
+
+plt.xlabel("Time t")
+plt.ylabel("Velocity v(t)")
+plt.text(45, 50, f"Terminal velocity: {v_t:.2f}")
+plt.plot(t, v_list)
+plt.savefig("v_plot.png")
+plt.show()
